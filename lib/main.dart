ef85b560f576e10a79d2bffa04d7b3e25f13bb97
arif-pandu <mapanduprayoga@gmail.com>
2023-10-24 10:18:44 +0700

    Clean Up :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:async' as sync;
 import 'dart:convert';
 import 'dart:math';
 import 'package:flame/components.dart';
@@ -7,7 +8,6 @@ import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
-import 'dart:async' as sync;
 import 'package:flutter/services.dart';
 
 void main() {
@@ -15,15 +15,15 @@ void main() {
     GameWidget(
       game: GameplayFlame(),
       overlayBuilderMap: {
-        "ButtonPause": (context, GameplayFlame game) {
+        'ButtonPause': (ctx, GameplayFlame g) {
           return Align(
             alignment: Alignment.topLeft,
             child: Container(
               margin: const EdgeInsets.only(top: 40, left: 10),
               child: GestureDetector(
-                onTap: () => game.pauseGame(),
+                onTap: () => g.pauseGame(),
                 child: Image.asset(
-                  "assets/images/pause-button.png",
+                  'assets/images/pause-button.png',
                   fit: BoxFit.cover,
                   height: 32,
                 ),
@@ -31,14 +31,14 @@ void main() {
             ),
           );
         },
-        "Overlay": (context, GameplayFlame game) {
+        'Overlay': (ctx, GameplayFlame g) {
           return Center(
             child: GestureDetector(
-              onTap: () => game.isGameOver ? game.resetGame() : game.resumeGame(),
+              onTap: () => g.isGameOver ? g.resetGame() : g.resumeGame(),
               child: Image.asset(
-                "assets/images/${game.isGameOver ? "gameover" : "pause"}-overlay.png",
+                "assets/images/${g.isGameOver ? "gameover" : "pause"}-overlay.png",
                 fit: BoxFit.cover,
-                height: game.size.x * .6,
+                height: g.size.x * .6,
               ),
             ),
           );
@@ -70,21 +70,19 @@ class GameplayFlame extends FlameGame with PanDetector, HasCollisionDetection {
 
   @override
   FutureOr<void> onLoad() async {
+    prepareJson();
     gateSprite = await loadSprite('gate.png');
     gateSize = Vector2(size.x * .36, size.x * .2);
     gateSizeExpand = Vector2(size.x * .49, size.x * .49 * 20 / 36) * 1.5;
-    overlays.addAll(["ButtonPause"]);
-    prepareJson();
-
+    overlays.addAll(['ButtonPause']);
     add(
       SpriteComponent(
-        sprite: Sprite(await images.load("bg.png")),
+        sprite: Sprite(await images.load('bg.png')),
         position: size / 2,
         size: size,
         anchor: Anchor.center,
       ),
     );
-
     questionText = TextComponent(
       size: Vector2(size.x * .4, size.y * .2),
       position: Vector2(size.x / 2, size.y * .1),
@@ -92,23 +90,25 @@ class GameplayFlame extends FlameGame with PanDetector, HasCollisionDetection {
     );
 
     scoreText = TextComponent()
-      ..text = "0"
+      ..text = '0'
       ..size = Vector2(size.x * .1, size.x * .1)
       ..position = Vector2(size.x - 10, size.y * .1)
       ..anchor = Anchor.centerRight;
 
     player = SpriteAnimationComponent(
       animation: await loadSpriteAnimation(
-        "player-sprite.png",
-        SpriteAnimationData.sequenced(amount: 8, stepTime: .1, textureSize: Vector2.all(256)),
+        'player-sprite.png',
+        SpriteAnimationData.sequenced(
+          amount: 8,
+          stepTime: .1,
+          textureSize: Vector2.all(256),
+        ),
       ),
       position: Vector2(size.x / 2, size.y - size.x * .25),
       size: Vector2.all(size.x * .25),
       anchor: Anchor.center,
       priority: 999,
     );
-
-    add(FpsTextComponent(position: Vector2(size.x, size.y * .1 + 20), anchor: Anchor.topRight, scale: Vector2.all(.5)));
     addAll([player, questionText, scoreText]);
     startSpawner();
   }
@@ -130,11 +130,17 @@ class GameplayFlame extends FlameGame with PanDetector, HasCollisionDetection {
     }
   }
 
-  void prepareJson() async {
-    String jsonData = await rootBundle.loadString('assets/json/question.json');
-    Map<String, dynamic> jsonMap = json.decode(jsonData);
+  Future<void> prepareJson() async {
+    final jsonData = await rootBundle.loadString('assets/json/question.json');
+    final jsonMap = json.decode(jsonData) as Map<String, dynamic>;
     questionsData = (jsonMap['question'] as List<dynamic>)
-        .map((questionData) => (questionData as List<dynamic>).map((answer) => answer.toString()).toList())
+        .map(
+          (questionData) => (questionData as List<dynamic>)
+              .map(
+                (answer) => answer.toString(),
+              )
+              .toList(),
+        )
         .toList();
   }
 
@@ -142,9 +148,8 @@ class GameplayFlame extends FlameGame with PanDetector, HasCollisionDetection {
     spawnGates();
     gateSpawner = Timer(
       intervalGate - 3,
-      onTick: () => spawnGates(),
+      onTick: spawnGates,
       repeat: true,
-      autoStart: true,
     );
   }
 
@@ -154,37 +159,41 @@ class GameplayFlame extends FlameGame with PanDetector, HasCollisionDetection {
       isFirstSpawn = false;
       questionText.text = questionsData[randomIndex][0];
     }
-
-    add(GateObject(
-        isTheAnswer: questionsData[randomIndex][3] == "left",
+    add(
+      GateObject(
+        isTheAnswer: questionsData[randomIndex][3] == 'left',
         theSprite: gateSprite,
         initPosition: Vector2((size.x / 2) - 5, size.y / 2),
         initSize: gateSize * .5,
         isLeft: true,
-        expandSize: gateSizeExpand,
+        ex: gateSizeExpand,
         intervalGate: intervalGate,
-        displayText: questionsData[randomIndex][1]));
-
-    add(GateObject(
-        isTheAnswer: questionsData[randomIndex][3] == "right",
+        displayText: questionsData[randomIndex][1],
+      ),
+    );
+    add(
+      GateObject(
+        isTheAnswer: questionsData[randomIndex][3] == 'right',
         theSprite: gateSprite,
         initPosition: Vector2((size.x / 2) + 5, size.y / 2),
         initSize: gateSize * .5,
         isLeft: false,
-        expandSize: gateSizeExpand,
+        ex: gateSizeExpand,
         intervalGate: intervalGate,
-        displayText: questionsData[randomIndex][2]));
+        displayText: questionsData[randomIndex][2],
+      ),
+    );
   }
 
   void gameover() {
-    overlays.add("Overlay");
+    overlays.add('Overlay');
     isGameOver = true;
     pauseEngine();
   }
 
   void resetGame() {
     isGameOver = false;
-    scoreText.text = "0";
+    scoreText.text = '0';
     gateSpawner.reset();
     player.position.x = size.x / 2;
     isFirstSpawn = true;
@@ -192,14 +201,14 @@ class GameplayFlame extends FlameGame with PanDetector, HasCollisionDetection {
 
   void pauseGame() {
     pauseEngine();
-    overlays.remove("ButtonPause");
-    overlays.add("Overlay");
+    overlays.remove('ButtonPause');
+    overlays.add('Overlay');
   }
 
   void resumeGame() {
     resumeEngine();
-    overlays.add("ButtonPause");
-    overlays.remove("Overlay");
+    overlays.add('ButtonPause');
+    overlays.remove('Overlay');
   }
 
   void answerCorrect() {
@@ -215,7 +224,7 @@ class GateObject extends SpriteComponent with HasGameRef<GameplayFlame> {
     required Vector2 initPosition,
     required Vector2 initSize,
     required this.isLeft,
-    required this.expandSize,
+    required this.ex,
     required this.intervalGate,
     required this.displayText,
     required this.isTheAnswer,
@@ -225,13 +234,8 @@ class GateObject extends SpriteComponent with HasGameRef<GameplayFlame> {
           size: initSize,
           anchor: isLeft ? Anchor.centerRight : Anchor.centerLeft,
         );
-
-  @override
-  bool get debugMode => false;
-
-  late EffectController effectController;
-
-  final Vector2 expandSize;
+  late EffectController ec;
+  final Vector2 ex;
   final double intervalGate;
   final bool isLeft;
   bool isCollide = false;
@@ -241,64 +245,54 @@ class GateObject extends SpriteComponent with HasGameRef<GameplayFlame> {
 
   @override
   FutureOr<void> onLoad() {
-    effectController = EffectController(
+    ec = EffectController(
       duration: intervalGate * 5,
       curve: Curves.fastOutSlowIn,
     );
-    textComponent = TextComponent(
-      text: displayText,
-      textRenderer: TextPaint(style: const TextStyle(fontSize: 12, color: Color(0xff25253D))),
-      anchor: Anchor.center,
-      position: size / 2,
-    );
-
-    add(SizeEffect.to(expandSize, effectController));
-
-    add(MoveEffect.by(
-      Vector2(0, gameRef.size.y / 2),
-      effectController,
-    ));
-
+    textComponent = TextComponent()
+      ..textRenderer = TextPaint(
+        style: const TextStyle(
+          color: Color(0xff25253D),
+        ),
+      )
+      ..text = displayText
+      ..anchor = Anchor.center
+      ..position = size / 2;
+    add(SizeEffect.to(ex, ec));
+    add(MoveEffect.by(Vector2(0, gameRef.size.y / 2), ec));
     add(textComponent);
-    textComponent.add(ScaleEffect.to(Vector2(expandSize.x / size.x, expandSize.y / size.y), effectController));
-
+    textComponent.add(
+      ScaleEffect.to(Vector2(ex.x / size.x, ex.y / size.y), ec),
+    );
     return super.onLoad();
   }
 
   @override
   void update(double dt) {
     textComponent.position = size / 2;
-    if (effectController.progress > 0.75) {
+    if (ec.progress > 0.75) {
       removeFromParent();
     }
-
-    if (effectController.progress > 0.7 && !isCollide) {
-      checkCollision();
-    }
-    super.update(dt);
-  }
-
-  void checkCollision() {
-    isCollide = true;
-    priority = 1000;
-
-    bool isInRangeLeft = gameRef.player.absoluteCenter.x < (gameRef.size.x / 2) - expandSize.x / 6;
-    bool isInRangeRight = gameRef.player.absoluteCenter.x > (gameRef.size.x / 2) + expandSize.x / 6;
-    bool isLeftSided = gameRef.player.absoluteCenter.x <= gameRef.size.x / 2;
-    if (isLeft && isLeftSided) {
-      if (isInRangeLeft || isInRangeRight) {
-        if (isTheAnswer) {
-          game.answerCorrect();
+    if (ec.progress > 0.7 && !isCollide) {
+      isCollide = true;
+      priority = 1000;
+      final playerAbs = gameRef.player.absoluteCenter.x;
+      final l = playerAbs < (gameRef.size.x / 2) - ex.x / 6;
+      final r = playerAbs > (gameRef.size.x / 2) + ex.x / 6;
+      final isLeftSided = gameRef.player.absoluteCenter.x <= gameRef.size.x / 2;
+      if (isLeft && isLeftSided) {
+        if (l || r) {
+          if (isTheAnswer) {
+            game.answerCorrect();
+          } else {
+            game.gameover();
+          }
         } else {
           game.gameover();
         }
-      } else {
-        game.gameover();
       }
+      sync.Timer(const Duration(milliseconds: 200), removeFromParent);
     }
-
-    sync.Timer(const Duration(milliseconds: 200), () {
-      removeFromParent();
-    });
+    super.update(dt);
   }
 }
